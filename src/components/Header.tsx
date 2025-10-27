3ec1e73f02e48a5eeeb2b88473021d7cd5f9f164
lucas soares <lucassoaresti@yahoo.com>
2025-10-27 20:44:09 -0300

    feat: added schedule tab with status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,26 @@
 import { mainContext } from "./../context/MainContext";
 import { useContext } from "react";
+import { ArrowDown } from "lucide-react";
 
 function Header() {
   const contexto = useContext(mainContext);
   if (!contexto) return null;
-  const { HeaderLogo, headerTitle, HeaderAdress, HeaderMinimalOrder } =
-    contexto;
+  const {
+    HeaderLogo,
+    headerTitle,
+    HeaderAdress,
+    HeaderMinimalOrder,
+    isScheduleOpen,
+    setisScheduleOpen,
+    Status,
+    Schedule,
+    floatParaHorario,
+  } = contexto;
   return (
     <div>
       <div className="grid grid-cols-2">
         <div className="flex">
-          <div className="flex  gap-2">
+          <div className="flex items-start gap-2">
             <img className="rounded-full w-[150px]" src={HeaderLogo} alt="" />
             <div className="flex flex-col gap-2  ">
               <h1 className="text-2xl poppins ">{headerTitle}</h1>
@@ -23,7 +33,49 @@ function Header() {
             </div>
           </div>
         </div>
-        <div className="bg-amber-500">2</div>
+        <div className="">
+          <div>
+            <h1
+              className={
+                Status.toLowerCase() === "funcionando"
+                  ? "poppins text-green-600"
+                  : "poppins text-red-500"
+              }
+            >
+              status: {Status}
+            </h1>{" "}
+            <button
+              onClick={() => {
+                setisScheduleOpen(!isScheduleOpen);
+              }}
+              className="flex justify-center w-full gap-2 border rounded-md hover:bg-amber-300 poppins"
+            >
+              Ver horarios
+              <ArrowDown />
+            </button>
+            <div className={isScheduleOpen ? "border rounded-md" : "hidden"}>
+              <div className="justify-center flex">
+                <h1 className="font-extrabold poppins">
+                  Horarios de funcionamento:
+                </h1>
+              </div>
+              {Schedule.map((a) => {
+                return (
+                  <div className="bg-slate-300 p-1 ">
+                    <h1 className="poppins font-bold">
+                      {a.fromday + "-" + a.today}
+                    </h1>
+                    <h1 className="poppins">
+                      {floatParaHorario(a.fromhour) +
+                        "-" +
+                        floatParaHorario(a.tohour)}{" "}
+                    </h1>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -6,12 +6,18 @@ import type { ReactNode } from "react";
 interface Exemple {
   exemple: string;
 }
-
+interface Horarios {
+  fromday: string;
+  today: string;
+  fromhour: number;
+  tohour: number;
+}
 //aqui voce declara todas os tipos das states e funções que voce criar
 type MainContextType = {
   exempleList: Exemple[];
   setexempleList: React.Dispatch<React.SetStateAction<Exemple[]>>;
   _delete: (id: number) => void;
+  floatParaHorario: (valor: number) => string;
   teste: string;
   setteste: React.Dispatch<React.SetStateAction<string>>;
   HeaderLogo: string;
@@ -22,6 +28,12 @@ type MainContextType = {
   setHeaderMinimalOrder: React.Dispatch<React.SetStateAction<string>>;
   HeaderAdress: string;
   setHeadderAdress: React.Dispatch<React.SetStateAction<string>>;
+  isScheduleOpen: boolean;
+  setisScheduleOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  Status: string;
+  setStatus: React.Dispatch<React.SetStateAction<string>>;
+  Schedule: Horarios[];
+  setSchedule: React.Dispatch<React.SetStateAction<Horarios[]>>;
 };
 
 //aqui você exporta o context tipado ou nulo
@@ -46,6 +58,38 @@ export function MainProvider({ children }: MainProviderProps) {
   const [HeaderMinimalOrder, setHeaderMinimalOrder] = useState<string>(
     "Pedido Minimo 18,00"
   );
+  const [isScheduleOpen, setisScheduleOpen] = useState<boolean>(false);
+  const [Status, setStatus] = useState<string>("Funcionando");
+  const [Schedule, setSchedule] = useState<Horarios[]>([
+    {
+      fromday: "segunda",
+      today: "quarta",
+      fromhour: 18,
+      tohour: 21,
+    },
+    {
+      fromday: "quinta",
+      today: "sexta",
+      fromhour: 18,
+      tohour: 23,
+    },
+    {
+      fromday: "sábado",
+      today: "domingo",
+      fromhour: 12,
+      tohour: 20,
+    },
+  ]);
+
+  function floatParaHorario(valor: number): string {
+    const hora = Math.floor(valor);
+    const minuto = Math.round((valor - hora) * 100);
+
+    const h = hora.toString().padStart(2, "0");
+    const m = minuto.toString().padStart(2, "0");
+
+    return `${h}:${m}`;
+  }
 
   function _delete(id: number) {
     console.log("deletado" + id);
@@ -67,6 +111,13 @@ export function MainProvider({ children }: MainProviderProps) {
         setHeadderAdress,
         HeaderMinimalOrder,
         setHeaderMinimalOrder,
+        isScheduleOpen,
+        setisScheduleOpen,
+        Status,
+        setStatus,
+        Schedule,
+        setSchedule,
+        floatParaHorario,
       }}
     >
       {children}
