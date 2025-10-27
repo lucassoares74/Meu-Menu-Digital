e132cad8238582bad15416ad573017908cef22c1
lucas soares <lucassoaresti@yahoo.com>
2025-10-27 19:30:46 -0300

    feat: added user info states

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,24 @@
+import { mainContext } from "./../context/MainContext";
+import { useContext } from "react";
+
 function Header() {
+  const contexto = useContext(mainContext);
+  if (!contexto) return null;
+  const { HeaderLogo, headerTitle, HeaderAdress, HeaderMinimalOrder } =
+    contexto;
   return (
     <div>
       <div className="grid grid-cols-2">
-        <div className="bg-amber-400 flex">
+        <div className="flex">
           <div className="flex  gap-2">
-            <img
-              className="rounded-full w-[150px]"
-              src="https://placehold.co/400"
-              alt=""
-            />
+            <img className="rounded-full w-[150px]" src={HeaderLogo} alt="" />
             <div className="flex flex-col gap-2  ">
-              <h1 className="text-2xl poppins ">Sabor da Massa</h1>
+              <h1 className="text-2xl poppins ">{headerTitle}</h1>
               <p className="text-[10px] tracking-tight poppins">
-                Rua Fernando Leite Mendes, 105 - São Sebastião Ilhéus BA
-                45653-778
+                {HeaderAdress}
               </p>
               <p className="text-[10px] tracking-tight poppins">
-                Pedido minimo: 18,00
+                {HeaderMinimalOrder}
               </p>
             </div>
           </div>
--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -14,6 +14,14 @@ type MainContextType = {
   _delete: (id: number) => void;
   teste: string;
   setteste: React.Dispatch<React.SetStateAction<string>>;
+  HeaderLogo: string;
+  setHeaderLogo: React.Dispatch<React.SetStateAction<string>>;
+  headerTitle: string;
+  setHeaderTitle: React.Dispatch<React.SetStateAction<string>>;
+  HeaderMinimalOrder: string;
+  setHeaderMinimalOrder: React.Dispatch<React.SetStateAction<string>>;
+  HeaderAdress: string;
+  setHeadderAdress: React.Dispatch<React.SetStateAction<string>>;
 };
 
 //aqui você exporta o context tipado ou nulo
@@ -28,17 +36,38 @@ export function MainProvider({ children }: MainProviderProps) {
     { exemple: "exemplo" },
   ]);
   const [teste, setteste] = useState<string>("teste");
-  const [HeaderLogo, setHeaderLogo] = useState<string>("https://placehold.co/400");
+  const [HeaderLogo, setHeaderLogo] = useState<string>(
+    "https://placehold.co/400"
+  );
   const [headerTitle, setHeaderTitle] = useState<string>("Sabor Da Massa");
-  const [HeaderAdress, setHeadderAdress] = useState<string>();
-  const [HeaderMinimalOrder, setHeaderMinimalOrder] = useState();
+  const [HeaderAdress, setHeadderAdress] = useState<string>(
+    "Rua Fernando Leite Mendes, 105 - São Sebastião Ilhéus BA45653-778"
+  );
+  const [HeaderMinimalOrder, setHeaderMinimalOrder] = useState<string>(
+    "Pedido Minimo 18,00"
+  );
+
   function _delete(id: number) {
     console.log("deletado" + id);
   }
 
   return (
     <mainContext.Provider
-      value={{ exempleList, setexempleList, _delete, teste, setteste }}
+      value={{
+        exempleList,
+        setexempleList,
+        _delete,
+        teste,
+        setteste,
+        HeaderLogo,
+        setHeaderLogo,
+        headerTitle,
+        setHeaderTitle,
+        HeaderAdress,
+        setHeadderAdress,
+        HeaderMinimalOrder,
+        setHeaderMinimalOrder,
+      }}
     >
       {children}
     </mainContext.Provider>
